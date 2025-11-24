bbc6ac5cf480d1209adc5830a778ce900550e6b7
Fabian Andres Marin Montoya <fabianandres.marin01@gmail.com>
2025-11-24 09:41:19 -0500

    feat: componente CabeceraCV con datos personales actualizados y foto de perfil

---
+++ src/components/CabeceraCV.jsx
@@ -0,0 +1,25 @@
+export default function CabeceraCV() {
+  return (
+    <header className="cabecera">
+
+      <img 
+        src="/fotoCV.jpg" alt="Foto de Fabián Marín" className="foto-perfil"
+      />
+
+      <h1>Fabián Marín</h1>
+      <h2>Desarrollador Web Jr. | Estudiante de ADSO</h2>
+
+      <p>
+        📍 Medellín – Calle 123 #45-67 <br />
+        📞 311 339 5127 <br />
+        📧 fabianandres.marin01@gmail.com
+      </p>
+
+      <p>
+        Documento: 1038062797 <br />
+        Fecha de nacimiento: 29 de diciembre de 2007
+      </p>
+
+    </header>
+  );
+}
